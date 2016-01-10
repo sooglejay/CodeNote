ee77e6329bca5d863bd648ff3721f37e313fefee
sooglejay <sooglejay@gmail.com>
2016-01-11 02:56:02 +0800

    two sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -4,6 +4,19 @@ import java.util.List;
 
 /**
  * Created by sooglejay on 16/1/11.
+ *
+ * LeetCode:Two Sum
+ *
+ * Question：
+ *
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
  */
 public class TwoSum {
     public static void main(String[] args) {
